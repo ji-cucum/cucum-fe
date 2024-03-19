6272f8097fa92e15ee2db77d296543de91fb76bd
hangyuCho <chg3376@gmail.com>
2024-03-19 10:12:31 +0900

    fix : add youtube player

---
+++ src/components/CucumRangeHorizontal.vue
@@ -0,0 +1,14 @@
+<template>
+  <input id="default-range" type="range" :value="value" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+</template>
+<script>
+export default {
+  name: 'CucumRange',
+  props: {
+    value: {
+      type: Number,
+      default: 50
+    }
+  },
+}
+</script>
